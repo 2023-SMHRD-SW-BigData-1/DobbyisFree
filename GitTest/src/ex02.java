5773781d93d5709c30cef48dc40ccb3ed4cdf248
smhrd <smhrd@DESKTOP-JP7VPJ1>
2023-05-09 11:45:10 +0900

    수정완

--- GitTest/src/ex02.java
+++ GitTest/src/ex02.java
@@ -5,7 +5,8 @@ public class ex02 {
 		System.out.println("여진");
 		System.out.println("병인");
 		System.out.println("경표");
-		System.out.println("이곳에 팀장님 이름 적어서 수정해주세요");
+		System.out.println("동하");
+		
 	}
 
 }
