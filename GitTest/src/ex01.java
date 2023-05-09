8e290d9eb2214976cc4eeca23a14a028d67ebd45
smhrd <smhrd@DESKTOP-JP7VPJ1>
2023-05-09 11:33:28 +0900

    집가고싶다

--- GitTest/src/ex01.java
+++ GitTest/src/ex01.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class ex01 {
 
@@ -7,7 +8,28 @@ public class ex01 {
 		System.out.println("팀원1 두번째 커밋");
 		System.out.println("팀원2 3번째 커밋?");
 		System.out.println("팀원3 4번째 커밋~");
+		System.out.println("집가고싶다");
 		
+		
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		if(num % 2 == 0) {
+			System.out.println("짝수입니다");
+			
+		}
+		if(num % 2 != 0) {
+			System.out.println("홀수입니다");
+		}
+		
+		
+		
+		
+		
+		
+
 	}
 
 }
