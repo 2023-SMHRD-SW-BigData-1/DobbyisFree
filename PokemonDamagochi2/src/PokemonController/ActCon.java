6bb870d324d6590a785d126803e7cb4b344570b3
YJ <YJ@DESKTOP-2HSR7KK>
2023-05-11 02:10:19 +0900

    ActCon,DAO,DTO 메소드 및 주석 업뎃 / Main 메인페이지 메소드 업뎃 / 각 파트별로 주석 내용 진행 부탁

--- PokemonDamagochi2/src/PokemonController/ActCon.java
+++ PokemonDamagochi2/src/PokemonController/ActCon.java
@@ -2,62 +2,188 @@ package PokemonController;
 
 import java.util.Scanner;
 
+import PokemonDAO.PokemonDAO;
+
 public class ActCon {
 	Scanner sc = new Scanner(System.in);
-	//시작 시 프롤로그
-	public void Start() { 
-	try {
-		for (int i = 0; i < 1; i++) {
-			System.out.println("포켓몬마스터가 되기 위해 모험을 떠난 '너!'와 친구들, 긴 여정을 마치고 환상의 포켓몬과의 최종결전을 앞두고 있다.");
-			Thread.sleep(1000);
-			 System.out.println("결전에 앞서 혹독하고 처절한 트레이닝을 계획하는데...");
+	PokemonDAO pdao = new PokemonDAO();
+
+	// 시작 시 프롤로그
+	public void Start() {
+		try {
+			for (int i = 0; i < 1; i++) {
+				System.out.println("포켓몬마스터가 되기 위해 모험을 떠난 '너!'와 친구들, 긴 여정을 마치고 환상의 포켓몬과의 최종결전을 앞두고 있다.");
+				Thread.sleep(1000);
+				System.out.println("결전에 앞서 혹독하고 처절한 트레이닝을 계획하는데...");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-	}catch(Exception e) {
-		System.out.println(e);
+
 	}
-	
-	}
-	
+
 	// 오박사님 등장시 대사
 	public void Doctorate() {
-	try {
-		for (int i = 0; i < 1; i++) {
-			System.out.println("그동안 고생많았다 이제 근본중에 근본인 그 녀석만 남았구나! 그간 함께했던 수많은 에이스포켓몬중에서 3마리를 엄선했단다.");
-			Thread.sleep(1000);
-			 System.out.println("아무리 그래도 다구리치는건 추잡하지 않니? 마지막으로 너와 함께할 포켓몬을 딱 1마리만 골라보렴");
+		try {
+			for (int i = 0; i < 1; i++) {
+				System.out.println("그동안 고생많았다 이제 근본중에 근본인 그 녀석만 남았구나! 그간 함께했던 수많은 에이스포켓몬중에서 3마리를 엄선했단다.");
+				Thread.sleep(1000);
+				System.out.println("아무리 그래도 다구리치는건 추잡하지 않니? 마지막으로 너와 함께할 포켓몬을 딱 1마리만 골라보렴");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-	}catch(Exception e) {
-		System.out.println(e);
 	}
-	}
-	
+
 	// 회원가입시 나오는 문구
 	public void New() {
-	      Scanner sc = new Scanner(System.in);
-	      System.out.print("ID 입력 : ");
-	      String ID = sc.next();
-	      System.out.print("PW 입력 : ");
-	      String PW = sc.next();
-	      
-	      System.out.print("함께할 Pokemon 선택 : ");
-	      String Pokemon = sc.next();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("ID 입력 : ");
+		String ID = sc.next();
+		System.out.print("PW 입력 : ");
+		String PW = sc.next();
+
+		System.out.print("함께할 Pokemon 선택 : ");
+		String Pokemon = sc.next();
 	}
-	
+
 	// 로그인시 나오는 문구
 	public void login() {
-	      System.out.print("ID 입력 : ");
-	      String ID = sc.next();
-	      System.out.print("PW 입력 : ");
-	      String PW = sc.next();
+		System.out.print("ID 입력 : ");
+		String ID = sc.next();
+		System.out.print("PW 입력 : ");
+		String PW = sc.next();
 	}
-	
+
 	// 채찍질
 	// 놀아주기
 	// 명상하기
-	
-	// 메인페이지 이동
-	
-	// 포켓몬 상태
-	
 
+	public void act() {
+		while (true) {
+			System.out.println("행동을 선택해주세요");
+			System.out.println("1.채찍질  2.놀아주기  3.잠자기");
+			int input = sc.nextInt();
+			
+			System.out.println("친밀도가 올랐다!");
+			// DB 능력치 값 불러오고 수정해서 다시 보내는 메소드
+			
+			// DB 능력치 값 불러오는 메소드 입력
+			
+			//.getLove  , .getExp 로 대조해야할 것 같은데 어떻게 로직 짜야하는지 조금만 더 생각해봄
+			// ArrayList로 담아야할것같다
+			// if(친밀도(getLove)>20 && 경험치(getExp)>20){
+			// 진화 메소드 실행 (스토리용 출력문 있어야함)
+			// 배틀 메소드 실행
+			// }else {
+			// 충족하지 못하면 act 첫줄부터 반복하게 비워두기
+			// }
+			
+			// DB 능력치 값 불러오는 메소드 입력
+			// 만약 HP가 30미만이라면 
+			// 1번,2번 선택시에 HP가 부족합니다
+			// act 첫줄부터 반복하게 비워두기
+			
+		}
+	}
+
+	// 메인페이지
+	public void main() {
+		int input = 0;
+
+		while (input != 3) {
+			System.out.println("메인 페이지"); // POKEMON 로고 메소드 불러와주세요
+			System.out.print("1. 회원가입 2. 로그인 3. 종료 ");
+			System.out.print("선택 >>  ");
+			input = sc.nextInt();
+
+			switch (input) {
+			case 1:
+				System.out.println("회원가입을 선택하셨습니다.");
+				System.out.println("오박사 문구"); // 문구 메소드 불러와주세요
+				System.out.print("ID 입력> ");
+				String id = sc.next();
+				System.out.print("PW 입력> ");
+				int pw = sc.nextInt();
+				System.out.println();
+
+				int pcode = 0;
+
+				while (pcode != 3) {
+					System.out.println("포켓몬을 선택하세요");
+					System.out.println("포켓몬그림"); // 포켓몬 그림 메소드 불러와주세요 
+					System.out.print("1.피카츄  2.이브이  3.치코리타");
+					pcode = sc.nextInt();
+					switch (pcode) {
+					case 1:
+						pdao.insertTrainer(id, pw, pcode);
+						System.out.println("게임을 시작합니다");
+						System.out.println();
+						//gameStart 메소드
+						break;
+
+					case 2:
+						pdao.insertTrainer(id, pw, pcode);
+						System.out.println("게임을 시작합니다");
+						System.out.println();
+						//gameStart 메소드
+						break;
+
+					case 3:
+						pdao.insertTrainer(id, pw, pcode);
+						System.out.println("게임을 시작합니다");
+						System.out.println();
+						//gameStart 메소드
+						break;
+
+					default:
+						System.out.println("잘못 입력하셨습니다.");
+						System.out.println("포켓몬 선택창으로 이동합니다.");
+						System.out.println();
+						break;
+
+					}
+
+				}
+
+				break;
+			case 2:
+				System.out.println("로그인 선택하셨습니다.");
+				System.out.print("ID 입력 > ");
+				id = sc.next();
+				System.out.print("PW 입력 > ");
+				pw = sc.nextInt();
+				// 로그인 DB 가져오는 메소드 (SELECT)
+				int cnt = 0;
+				if (cnt > 0) { //cnt는 로그인 성공 여부 값을 받아오는 변수
+					// 로그인에 성공한다면 gameStart 메소드 실행
+					
+				}else {
+					//로그인 정보가 틀렸다는 내용 출력
+					System.out.println("로그인 정보를 다시 확인해주세요. 메인화면으로 이동합니다.");
+					System.out.println();
+				}
+
+				break;
+			case 3:
+				System.out.println("게임을 종료합니다.");
+				break;
+			default:
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+				System.out.println();
+				
+				break;
+
+			}
+		}
+	}
+
+	// 포켓몬 상태
+	// select 메소드로 불러와서
+	// 이름 , hp , atk , love , exp만 출력
+	
+	
+	// 진화창
+
+	
 }
--- PokemonDamagochi2/src/View/Main.java
+++ PokemonDamagochi2/src/View/Main.java
@@ -3,60 +3,76 @@ package View;
 import java.util.Scanner;
 
 import PokemonController.ActCon;
-
-
+import PokemonDAO.PokemonDAO;
 
 public class Main {
-	
+
+	static PokemonController.ActCon pcon = new PokemonController.ActCon();
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
+		PokemonDAO pdao = new PokemonDAO();
+
+		pcon.main();
+
+		pcon.act();
+		// 친밀도 == 20 && 경험치 == 20 이면 진화창 출력
+		// 배틀메소드 실행
+
+//			System.out.println("게임종료를 선택하셨습니다.");
+//			System.out.println("게임을 종료합니다.");
+//			// 게임종료메소드
+//	
+//
+//			System.out.println("잘못 입력 하셨습니다.");
+//			System.out.println("메인화면으로 이동합니다.");
+
 		// 출력문 입력 부탁드립니다.
 		ActCon SM = new ActCon();
 		SM.Start();
 
-		 
-		//오박사 등장
+		// 오박사 등장
 		SM.Doctorate();
-		
-		//회원가입 및 포켓몬 선택 
+
+		// 회원가입 및 포켓몬 선택
 		SM.New();
-		
-		//로그인
+
+		// 로그인
 		SM.login();
-		
-		
-		
-		//키우기
+
+		// 키우기
 		System.out.println("1.행동 선택   2.메인페이지");
 		int start = sc.nextInt();
-		
+
 		System.out.println("<< 1.채찍질하기 2.놀아주기 3.잠자기 >> ");
 		int training = sc.nextInt();
-		
+
 		System.out.println("---는 기분 좋은듯하다^^ 공격력이 올랐다!");
 		System.out.println("경험치가 올랐다.");
-		
+
 		System.out.println("하하하하하하하~호호호호호호~흐헤흐헤흐히흐히히히히~ 친밀도가 올랐다!");
 		System.out.println("경험치가 올랐다.");
-		
+
 		System.out.println("우리 ---는 자는 모습이 제일 귀엽당>< HP가 회복되었다!");
 		System.out.println("경험치가 올랐다.");
-		
-	 System.out.println("오잉?! ---의 상태가?");
-	 System.out.println("---는 ---로 진화했다!");
-	 
-	 //최종배틀
-	 System.out.println("포켓몬 전투병기 뮤츠가 나타났다!");
-	 
-	 System.out.println("뮤츠와의 배틀에서 승리하였습니다!");
-	 
-	 System.out.println("뮤츠와 친구가 되었다! ID는 포켓몬마스터가 되었다");
-	 
-	 System.out.println("어머! ---는 싸우는 사이 뮤츠와 정분이 났다! ♡♡♡");
-	 
-	 //패배
-	 System.out.println("패배하였습니다... 채찍질이 부족한 것 같다"); //돌아가기
-	 
+
+		System.out.println("오잉?! ---의 상태가?");
+		System.out.println("---는 ---로 진화했다!");
+
+		// 최종배틀
+		System.out.println("포켓몬 전투병기 뮤츠가 나타났다!");
+
+		System.out.println("뮤츠와의 배틀에서 승리하였습니다!");
+
+		System.out.println("뮤츠와 친구가 되었다! ID는 포켓몬마스터가 되었다");
+
+		System.out.println("어머! ---는 싸우는 사이 뮤츠와 정분이 났다! ♡♡♡");
+
+		// 패배
+		System.out.println("패배하였습니다... 채찍질이 부족한 것 같다");
+		// 돌아가기
+
 	}
 
 }
